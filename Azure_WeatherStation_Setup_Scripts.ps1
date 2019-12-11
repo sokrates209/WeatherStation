f2db22173ceb37d8b184afee0cc875e80c50defb
sokrates209 <34916170+sokrates209@users.noreply.github.com>
2019-12-11 10:16:24 +0100

    PS script to setup azure iot hub

---
+++ Azure_WeatherStation_Setup_Scripts.ps1
@@ -0,0 +1,27 @@
+﻿# C:\Users\Tomek\Desktop\Azure_ctx.json - context file to use with azure powershell session
+
+#login
+$account = Import-AzureRmContext -Path C:\Users\Tomek\Desktop\Azure_ctx.json
+$account
+
+#get subscription
+$subscription = Get-AzureRmSubscription -SubscriptionName EcoVadis-ITD-DEV
+$subscription
+
+#try get resource group for IOT if not existing create
+$resourceGroup = Get-AzureRmResourceGroup -Location "West US" -Name IOT
+if(-not $resourceGroup){
+    $resourceGroup = New-AzureRmResourceGroup -Location "West US" -Name IOT
+}
+$resourceGroup
+
+#try get IOT Hub if not existing create
+$iotHub = Get-AzureRmIotHub -Name IOT -ResourceGroupName IOT
+if(-not $iotHub){
+    $iotHub =  New-AzureRmIotHub -Location "West US" -Name EcoVadisWeatherStation -ResourceGroupName IOT -SkuName F1 -Units 1   
+}
+$iotHub
+
+#Get-AzureSubscription
+
+#Remove-AzureAccount 
